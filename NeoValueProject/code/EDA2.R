488c9bf454d7af00dcd7a65f7741fc999feb75b4
SeungjunLee <seungjun1588@yonsei.ac.kr>
2023-07-28 17:35:25 +0900

    add merge code:EDA2

--- NeoValueProject/code/EDA2.R
+++ NeoValueProject/code/EDA2.R
@@ -194,14 +194,18 @@ df %>%
 
 df %>% 
   mutate(MONTH = substr(SALE_DT,5,6),
-         YEAR = substr(SALE_DT,1,4)) %>% 
-  group_by(YEAR,MONTH,SHOP_NAME) %>% 
+         YEAR = substr(SALE_DT,1,4),
+         DATE = substr(SALE_DT,7,8),
+         newdate = paste0(MONTH,"-",DATE),
+         newdate = as.POSIXct(SALE_DT,format="%Y-%m-%d")) %>% 
+  group_by(YEAR,MONTH,date,SHOP_NAME) %>% 
   summarise(SHOP_NAME = first(SHOP_NAME),
             MEAN_SALE= mean(TOT_SALE_AMT)) %>% 
   ungroup() %>% 
   filter(SHOP_NAME =="플레이위드미") %>% 
-  ggplot() +
-  geom_line(aes(x=MONTH,y=MEAN_SALE,color=as.factor(YEAR)),alpha=0.5)
+  ggplot(aes(x=date,y=MEAN_SALE,group=YEAR)) +
+  ggtitle("플레이위드미")+
+  geom_line(aes(color=YEAR),alpha=0.4)
 
 #------------------------------------------------------------------#
 # 포인트적립(CRM.CR_POINT_ADD)
@@ -214,7 +218,7 @@ length(unique(MDPoint %>% select(APPR_ID) %>%  pull)) # 502473
 length(unique(MDPoint %>% select(SHOP_CD) %>%  pull)) # 122
 length(unique(MDPoint %>% select(CUST_ID) %>%  pull)) # 17031
 
-sum(!is.na(MDPoint$TRADE_CD))
+sum(!is.na(MDPoint$TRADE_CD)) 
 sum(!is.na(MDPoint$VEN_CD))
 
 MDPoint = MDPoint %>% select(-TRADE_CD,-VEN_CD)
@@ -264,10 +268,139 @@ left_join(SHOP_VISIT,SHOP_LIST,by=c("SHOP_CD")) %>%
   select(SHOP_CD,SHOP_LIST,n,MEAN_SALE) # SHOP_CD =0인건 확인안됨...
 
 # 경로 파악
+ 
+
+newdf = pivot_wider(df2,names_from = SHOP_CD,values_from=n)
+newdf %>% replace(.,is.na(.),0) -> newdf
+
+
+library(psych)
+faaa = fa(t(data.matrix(newdf)),nfactors=4,fm-"ml")
+
+clusters = kmeans(faaa$loadings,centers=4,iter.max=100)
+clusters$centers
+clusters$cluster
+
+# 방문 횟수와 사용 금액에 따라서 색상표시?
+
+MDPoint %>% 
+  group_by(CUST_ID) %>% 
+  summarise(n=n(),COST=sum(as.numeric(TRADE_AMT))) -> MDsumm
+
+
+clusters = kmeans(MDsumm[,2:3],centers=10,iter.max=1000)
+clusters$centers
+clusters$cluster
+
+MDsumm$cluster = clusters$cluster
+
+
+MDsumm %>% 
+  filter(COST < 2e+07) %>% 
+  ggplot() + 
+  geom_point(aes(x=COST,y=n,color=as.factor(cluster)))
+
+#---------------------------------#
+# n만 고려
+clusters = kmeans(MDsumm[,2],centers=4,iter.max=100)
+clusters$centers
+clusters$cluster
+
+MDsumm$cluster = clusters$cluster
+
+
+MDsumm %>% 
+  filter(COST < 2e+07) %>% 
+  ggplot() + 
+  geom_point(aes(x=COST,y=n,color=as.factor(cluster)))
+#---------------------------------#
+# 금액만 고려
+clusters = kmeans(MDsumm[,3],centers=4,iter.max=100)
+clusters$centers
+clusters$cluster
+
+MDsumm$cluster = clusters$cluster
+
+
+MDsumm %>% 
+  filter(COST < 2e+07) %>% 
+  ggplot() + 
+  geom_point(aes(x=COST,y=n,color=as.factor(cluster)))
+
+#---------------------------------------------------------------------#
+# 매출 데이터 통합
+#---------------------------------------------------------------------#
+pwd = "C:/Seungjun/neovalue/NEOVALUE/230616_네오밸류 데이터(발송용)/네오밸류 데이터/02_운영데이터 등/230531_광교 데이터/후지쯔 매출내역(Open~2210)/일별/매출건수"
+
+# year = 숫자, start = 시작월(숫자)
+MergedDataSet = function(year,start=1,end=12){
+  Year = paste0("/",year,"년")
+  file_lst = list.files(paste0(pwd,Year))
+
+  
+  # 2019년 4월에서12월까지만 병합.
+  sales19 = read_excel(paste0(pwd,Year,"/",file_lst[start]),skip=11) %>% 
+    slice(-1,-n()) %>% 
+    select(-"합계",-1) %>%
+    rename("SHOP_NAME"="...3") 
+  
+  makeyear = function(year,month,day){
+    days = (year-2000)*10000+month*100+day
+    return(days)
+  }
+  newcolnames = colnames(sales19)[-(1:2)] %>% as.numeric %>% sapply(makeyear,month=start,year=year)
+  colnames(sales19)[3:length(colnames(sales19))] = newcolnames
+  colnames(sales19)[1] = "SHOP_CD"
+  
+  for(month in (start+1):end){
+    # 해당 월 파일 가져오기
+    sales_month = read_excel(paste0(pwd,Year,"/",file_lst[month]),skip=11) %>% 
+      slice(-1,-n()) %>% 
+      select(-"합계",-1) %>%
+      rename("SHOP_NAME"="...3")
+    
+    # 열이름 변경
+    newcolnames = colnames(sales_month)[-(1:2)] %>% as.numeric %>% sapply(makeyear,year=year,month=month)
+    print(newcolnames)
+    print("*************************")
+    colnames(sales_month)[3:length(colnames(sales_month))] = newcolnames
+    colnames(sales_month)[1] = "SHOP_CD"
+    
+    # 병합
+    sales19 = merge(sales19,sales_month,by=c("SHOP_CD","SHOP_NAME"),all=TRUE)
+  }
+  return(sales19)
+}
+
+
+# 일별 매출건수
+sales19 = MergedDataSet(year=2019,start=4)
+sales20 = MergedDataSet(year=2020,start=1)
+sales21 = MergedDataSet(year=2021,start=1)
+sales22 = MergedDataSet(year=2022,start=1,end=10)
+TotalSalesNum = full_join(sales19,sales20,sales21,by=c("SHOP_CD","SHOP_NAME"))
+TotalSalesNum = full_join(TotalSalesNum,sales22,by=c("SHOP_CD","SHOP_NAME"))
+
+
+dim(TotalSalesNum)
+length(unique(TotalSalesNum$SHOP_CD))
+length(unique(TotalSalesNum$SHOP_NAME))
+
+# 이름은 같으나 코드가 달라진 가게(어떤 변화가 있었는지 확인 필요)
+TotalSalesNum %>% 
+  group_by(SHOP_NAME) %>% 
+  summarise(n=n()) %>% 
+  arrange(desc(n))
 
 
 
 
+
+
+
+
+View(tbl)
+View(df2)
 #------------------------------------------------------------------#
 # save
 library(openxlsx)
